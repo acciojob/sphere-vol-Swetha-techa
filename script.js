b216776539693545842c70cda312b04878036a1d
Swetha-techa <118538021+Swetha-techa@users.noreply.github.com>
2023-06-16 22:22:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere() {
     //Write your code here
 	const radius = document.getElementById('radius').value;
-	const volume = (4/3) * Math.PI * Math.pow(radius * 3);
+	const volume = (4/3) * Math.PI * Math.pow(radius , 3);
 	document.getElementById('volume').value = volume.toFixed(4);  
 } 
 
